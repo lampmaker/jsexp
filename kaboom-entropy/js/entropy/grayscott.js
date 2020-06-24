4432d1795d5d51c4030ba15289fcde6a1729c8b7
Matthijs Keuper <matthijs.keuper@invisua.com>
2020-06-24 11:48:55 +0200

    alles verplaatst; poging om shaders in externe js files te zetten. werkt nog niet

---
+++ kaboom-entropy/js/entropy/grayscott.js
@@ -0,0 +1,494 @@
+/* 
+ * Gray-Scott
+ *
+ * A solver of the Gray-Scott model of reaction diffusion.
+ *
+ * ©2012 pmneila.
+ * p.mneila at upm.es
+ */
+
+(function () {
+
+    // Canvas.
+    var canvas;
+    var canvasQ;
+    var canvasWidth;
+    var canvasHeight;
+
+    var mMouseX, mMouseY;
+    var mMouseDown = false;
+
+    var mRenderer;
+    var mScene;
+    var mCamera;
+    var mUniforms;
+    var mColors;
+    var mColorsNeedUpdate = true;
+    var mLastTime = 0;
+    var mClearMode = 2; /* First click will make it 3, which is the better simplex noise */
+    var mPaintMode = 0; /* First click will make it 1, which is to paint blue */
+
+    var mTexture1, mTexture2;
+    var mGSMaterial, mScreenMaterial;
+    var mScreenQuad;
+
+    var mToggled = false;
+
+    var mMinusOnes = new THREE.Vector2(-1, -1);
+
+    // Some presets.
+    var presets = [
+        { feed: 0.098, kill: 0.0555 }, // Negative bubbles (sigma)
+        { feed: 0.098, kill: 0.057 }, // Positive bubbles (rho)
+        { feed: 0.085, kill: 0.059 }, // Precritical bubbles (rho/kappa)
+        { feed: 0.082, kill: 0.060 }, // Worms and loops (kappa)
+        { feed: 0.074, kill: 0.064 }, // Stable solitons (nu)
+        { feed: 0.062, kill: 0.0609 }, // The U-Skate World (pi)
+        { feed: 0.058, kill: 0.065 }, // Worms (mu)
+        { feed: 0.046, kill: 0.063 }, // Worms join into maze (kappa)
+        { feed: 0.046, kill: 0.0594 }, // Negatons (iota)
+        { feed: 0.042, kill: 0.059 }, // Turing patterns (delta)
+        { feed: 0.039, kill: 0.058 }, // Chaos to Turing negatons (beta)
+        { feed: 0.037, kill: 0.06 }, // Fingerprints (theta/kappa)
+        { feed: 0.0353, kill: 0.0566 }, // Chaos with negatons (beta/delta)
+        { feed: 0.034, kill: 0.0618 }, // Spots and worms (eta)
+        { feed: 0.03, kill: 0.063 }, // Self-replicating spots (lambda)
+        { feed: 0.03, kill: 0.0565 }, // Super-resonant mazes (theta)
+        { feed: 0.029, kill: 0.057 }, // Mazes (kappa)
+        { feed: 0.026, kill: 0.055 }, // Mazes with some chaos (gamma)
+        { feed: 0.026, kill: 0.051 }, // Chaos (beta)
+        { feed: 0.025, kill: 0.06 }, // Pulsating solitons (zeta)
+        { feed: 0.022, kill: 0.059 }, // Warring microbes (epsilon)
+        { feed: 0.018, kill: 0.051 }, // Spots and loops (alpha)
+        { feed: 0.014, kill: 0.054 }, // Moving spots (alpha)
+        { feed: 0.014, kill: 0.045 }, // Waves (xi)
+
+        { feed: 0.001, kill: 0.03 } // leave this line at the end
+    ];
+
+    // Configuration.
+    var feed = presets[0].feed;
+    var kill = presets[0].kill;
+    var feed2 = feed;
+    var kill2 = kill;
+    var loader = new THREE.FileLoader();
+    var lmanager = new THREE.LoadingManager();
+    var shader_scrf, shader_stdf, shader_stdv;
+
+    loadshaders = function () {
+        loader.load("js/shaders/screenfragment.js", function (data) { shader_scrf = data; });
+        loader.load("js/shaders/standardfragment.js", function (data) { shader_stdf = data; });
+        loader.load("js/shaders/standardvertex.js", function (data) { shader_stv = data; });
+       // lmanager.onLoad = init();
+    };
+
+
+    init = function () {
+        init_controls();
+
+        canvasQ = $('#myCanvas');
+        canvas = canvasQ.get(0);
+
+        canvas.onmousedown = onMouseDown;
+        canvas.onmouseup = onMouseUp;
+        canvas.onmousemove = onMouseMove;
+
+        mRenderer = new THREE.WebGLRenderer({ canvas: canvas, preserveDrawingBuffer: true });
+
+        mScene = new THREE.Scene();
+        mCamera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, -10000, 10000);
+        mCamera.position.z = 100;
+        mScene.add(mCamera);
+
+        mUniforms = {
+            screenWidth: { type: "f", value: undefined },
+            screenHeight: { type: "f", value: undefined },
+            tSource: { type: "t", value: undefined },
+            delta: { type: "f", value: 1.0 },
+            feed: { type: "f", value: feed },
+            kill: { type: "f", value: kill },
+            feed2: { type: "f", value: feed2 },
+            kill2: { type: "f", value: kill2 },
+            brmode: { type: "f", value: 0.0 },
+            brush: { type: "v2", value: new THREE.Vector2(-10, -10) },
+            color1: { type: "v4", value: new THREE.Vector4(0, 0, 0.0, 0) },
+            color2: { type: "v4", value: new THREE.Vector4(0, 1, 0, 0.2) },
+            color3: { type: "v4", value: new THREE.Vector4(1, 1, 0, 0.21) },
+            color4: { type: "v4", value: new THREE.Vector4(1, 0, 0, 0.4) },
+            color5: { type: "v4", value: new THREE.Vector4(1, 1, 1, 0.6) }
+        };
+        mColors = [mUniforms.color1, mUniforms.color2, mUniforms.color3, mUniforms.color4, mUniforms.color5];
+        $("#gradient").gradient("setUpdateCallback", onUpdatedColor);
+        /*
+                mGSMaterial = new THREE.ShaderMaterial({
+                    uniforms: mUniforms,
+                    vertexShader: document.getElementById('standardVertexShader').textContent,
+                    fragmentShader: document.getElementById('gsFragmentShader').textContent,
+                });
+        */
+        mGSMaterial = new THREE.ShaderMaterial({
+            uniforms: mUniforms,
+            vertexShader: shader_stdv,
+            fragmentShader: shader_stdf,
+        });
+
+
+
+        mScreenMaterial = new THREE.ShaderMaterial({
+            uniforms: mUniforms,
+            vertexShader: shader_stdv,
+            fragmentShader: shader_scrf,            
+        });
+
+        var plane = new THREE.PlaneGeometry(1.0, 1.0);
+        mScreenQuad = new THREE.Mesh(plane, mScreenMaterial);
+        mScene.add(mScreenQuad);
+
+        mColorsNeedUpdate = true;
+
+        resize(canvas.clientWidth, canvas.clientHeight);
+
+        render(0);
+        mUniforms.brush.value = new THREE.Vector2(0.5, 0.5);
+        mLastTime = new Date().getTime();
+        requestAnimationFrame(render);
+    }
+
+    var resize = function (width, height) {
+        // Set the new shape of canvas.
+        canvasQ.width(width);
+        canvasQ.height(height);
+
+        // Get the real size of canvas.
+        canvasWidth = canvasQ.width();
+        canvasHeight = canvasQ.height();
+
+        mRenderer.setSize(canvasWidth, canvasHeight);
+
+        // TODO: Possible memory leak?
+        mTexture1 = new THREE.WebGLRenderTarget(canvasWidth / 2, canvasHeight / 2,
+            {
+                minFilter: THREE.LinearFilter,
+                magFilter: THREE.LinearFilter,
+                format: THREE.RGBAFormat,
+                type: THREE.FloatType
+            });
+        mTexture2 = new THREE.WebGLRenderTarget(canvasWidth / 2, canvasHeight / 2,
+            {
+                minFilter: THREE.LinearFilter,
+                magFilter: THREE.LinearFilter,
+                format: THREE.RGBAFormat,
+                type: THREE.FloatType
+            });
+        mTexture1.wrapS = THREE.RepeatWrapping;
+        mTexture1.wrapT = THREE.RepeatWrapping;
+        mTexture2.wrapS = THREE.RepeatWrapping;
+        mTexture2.wrapT = THREE.RepeatWrapping;
+
+        mUniforms.screenWidth.value = canvasWidth / 2;
+        mUniforms.screenHeight.value = canvasHeight / 2;
+    }
+
+    var render = function (time) {
+        var dt = (time - mLastTime) / 20.0;
+        if (dt > 0.8 || dt <= 0)
+            dt = 0.8;
+        mLastTime = time;
+
+        mScreenQuad.material = mGSMaterial;
+        mUniforms.delta.value = dt;
+        mUniforms.feed.value = feed;
+        mUniforms.kill.value = kill;
+        mUniforms.feed2.value = feed2;
+        mUniforms.kill2.value = kill2;
+        for (var i = 0; i < 8; ++i) {
+            if (!mToggled) {
+                mUniforms.tSource.value = mTexture1;
+                mRenderer.render(mScene, mCamera, mTexture2, true);
+                mUniforms.tSource.value = mTexture2;
+            }
+            else {
+                mUniforms.tSource.value = mTexture2;
+                mRenderer.render(mScene, mCamera, mTexture1, true);
+                mUniforms.tSource.value = mTexture1;
+            }
+
+            mToggled = !mToggled;
+            mUniforms.brush.value = mMinusOnes;
+        }
+
+        if (mColorsNeedUpdate)
+            updateUniformsColors();
+
+        mScreenQuad.material = mScreenMaterial;
+        mRenderer.render(mScene, mCamera);
+
+        requestAnimationFrame(render);
+    }
+
+    loadPreset = function (idx) {
+        feed = presets[idx].feed;
+        kill = presets[idx].kill;
+        worldToForm();
+    }
+
+    loadPreset2 = function (idx) {
+        feed2 = presets[idx].feed;
+        kill2 = presets[idx].kill;
+        worldToForm();
+    }
+
+    var updateUniformsColors = function () {
+        var values = $("#gradient").gradient("getValuesRGBS");
+        for (var i = 0; i < values.length; i++) {
+            var v = values[i];
+            mColors[i].value = new THREE.Vector4(v[0], v[1], v[2], v[3]);
+        }
+
+        mColorsNeedUpdate = false;
+    }
+
+    var onUpdatedColor = function () {
+        mColorsNeedUpdate = true;
+        updateShareString();
+    }
+
+
+
+    var onMouseMove = function (e) {
+        var ev = e ? e : window.event;
+
+        mMouseX = ev.pageX - canvasQ.offset().left; // these offsets work with
+        mMouseY = ev.pageY - canvasQ.offset().top; //  scrolled documents too
+
+        if (mMouseDown) {
+            // Send brush command to paint pixels near mouse position
+            mUniforms.brush.value = new THREE.Vector2(mMouseX / canvasWidth,
+                1 - mMouseY / canvasHeight);
+        }
+    }
+
+
+
+    var onMouseDown = function (e) {
+        var ev = e ? e : window.event;
+        e
+        mMouseDown = true;
+        mPaintMode = (mPaintMode + 1) % 2;
+        mUniforms.brmode.value = mPaintMode ? 1.0 : 0.0;
+
+        // Send brush command to paint pixels near mouse position
+        mUniforms.brush.value = new THREE.Vector2(mMouseX / canvasWidth,
+            1 - mMouseY / canvasHeight);
+    }
+
+    var onMouseUp = function (e) {
+        mMouseDown = false;
+    }
+
+    clean = function () {
+        mClearMode = (mClearMode + 1) % 5;
+        // Send brush command to erase screen
+        if (mClearMode == 0) {
+            // Clear to red state
+            mUniforms.brush.value = new THREE.Vector2(-10, -10);
+        } else if (mClearMode == 1) {
+            // Draw a single blue spot
+            mUniforms.brush.value = new THREE.Vector2(0.5, 0.5);
+            mUniforms.brmode.value = 1.0;
+        } else if (mClearMode == 2) {
+            // Clear to blue state
+            mUniforms.brush.value = new THREE.Vector2(-10, -1);
+        } else if (mClearMode == 3) {
+            // Use earlier simpler noise function
+            mUniforms.brush.value = new THREE.Vector2(-10, 2);
+        } else {
+            // Use noise with 2 degrees of freedom
+            mUniforms.brush.value = new THREE.Vector2(-10, 1);
+        }
+    }
+
+
+
+    snapshot = function () {
+        var dataURL = canvas.toDataURL("image/png");
+        window.open(dataURL, "name-" + Math.random());
+    }
+
+    // resize canvas to fullscreen, scroll to upper left 
+    // corner and try to enable fullscreen mode and vice-versa
+    fullscreen = function () {
+
+        var canv = $('#myCanvas');
+        var elem = canv.get(0);
+
+        if (isFullscreen()) {
+            // end fullscreen
+            if (elem.cancelFullscreen) {
+                elem.cancelFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        }
+
+        if (!isFullscreen()) {
+            // save current dimensions as old
+            window.oldCanvSize = {
+                width: canv.width(),
+                height: canv.height()
+            };
+
+            // adjust canvas to screen size
+            resize(screen.width, screen.height);
+
+            // scroll to upper left corner
+            $('html, body').scrollTop(canv.offset().top);
+            $('html, body').scrollLeft(canv.offset().left);
+
+            // request fullscreen in different flavours
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.webkitRequestFullscreen) {
+                elem.webkitRequestFullscreen();
+            }
+        }
+    }
+
+    var isFullscreen = function () {
+        return document.mozFullScreenElement ||
+            document.webkitCurrentFullScreenElement ||
+            document.fullscreenElement;
+    }
+
+    $(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (ev) {
+        // restore old canvas size
+        if (!isFullscreen())
+            resize(window.oldCanvSize.width, window.oldCanvSize.height);
+    });
+
+    var worldToForm = function () {
+        //document.ex.sldReplenishment.value = feed * 1000;
+        $("#sld_replenishment").slider("value", feed);
+        $("#sld_diminishment").slider("value", kill);
+    }
+
+    var init_controls = function () {
+        $("#sld_replenishment").slider({
+            value: feed, min: 0, max: 0.1, step: 0.001,
+            change: function (event, ui) { $("#replenishment").html(ui.value); feed = ui.value; updateShareString(); },
+            slide: function (event, ui) { $("#replenishment").html(ui.value); feed = ui.value; updateShareString(); }
+        });
+        $("#sld_replenishment").slider("value", feed);
+        $("#sld_diminishment").slider({
+            value: kill, min: 0, max: 0.073, step: 0.001,
+            change: function (event, ui) { $("#diminishment").html(ui.value); kill = ui.value; updateShareString(); },
+            slide: function (event, ui) { $("#diminishment").html(ui.value); kill = ui.value; updateShareString(); }
+        });
+        $("#sld_diminishment").slider("value", kill);
+
+        $("#sld_replenishment2").slider({
+            value: feed, min: 0, max: 0.1, step: 0.001,
+            change: function (event, ui) { $("#replenishment2").html(ui.value); feed2 = ui.value; updateShareString(); },
+            slide: function (event, ui) { $("#replenishment2").html(ui.value); feed2 = ui.value; updateShareString(); }
+        });
+        $("#sld_replenishment2").slider("value", feed);
+        $("#sld_diminishment2").slider({
+            value: kill, min: 0, max: 0.073, step: 0.001,
+            change: function (event, ui) { $("#diminishment2").html(ui.value); kill2 = ui.value; updateShareString(); },
+            slide: function (event, ui) { $("#diminishment2").html(ui.value); kill2 = ui.value; updateShareString(); }
+        });
+        $("#sld_diminishment2").slider("value", kill);
+
+
+
+
+        $('#share').keypress(function (e) {
+            if (e.which == 13) {
+                parseShareString();
+                return false;
+            }
+        });
+
+        $("#btn_clear").button({
+            icons: { primary: "ui-icon-document" },
+            text: false
+        });
+        $("#btn_snapshot").button({
+            icons: { primary: "ui-icon-image" },
+            text: false
+        });
+        $("#btn_fullscreen").button({
+            icons: { primary: "ui-icon-arrow-4-diag" },
+            text: false
+        });
+
+        $("#notworking").click(function () {
+            $("#requirement_dialog").dialog("open");
+        });
+        $("#requirement_dialog").dialog({
+            autoOpen: false
+        });
+    }
+
+    alertInvalidShareString = function () {
+        $("#share").val("Invalid string!");
+        setTimeout(updateShareString, 1000);
+    }
+
+    parseShareString = function () {
+        var str = $("#share").val();
+        var fields = str.split(",");
+
+        if (fields.length != 12) {
+            alertInvalidShareString();
+            return;
+        }
+
+        var newFeed = parseFloat(fields[0]);
+        var newKill = parseFloat(fields[1]);
+
+        if (isNaN(newFeed) || isNaN(newKill)) {
+            alertInvalidShareString();
+            return;
+        }
+
+        var newValues = [];
+        for (var i = 0; i < 5; i++) {
+            var v = [parseFloat(fields[2 + 2 * i]), fields[2 + 2 * i + 1]];
+
+            if (isNaN(v[0])) {
+                alertInvalidShareString();
+                return;
+            }
+
+            // Check if the string is a valid color.
+            if (! /^#[0-9A-F]{6}$/i.test(v[1])) {
+                alertInvalidShareString();
+                return;
+            }
+
+            newValues.push(v);
+        }
+
+        $("#gradient").gradient("setValues", newValues);
+        feed = newFeed;
+        kill = newKill;
+        worldToForm();
+    }
+
+    updateShareString = function () {
+        var str = "".concat(feed, ",", kill);
+
+        var values = $("#gradient").gradient("getValues");
+        for (var i = 0; i < values.length; i++) {
+            var v = values[i];
+            str += "".concat(",", v[0], ",", v[1]);
+        }
+        $("#share").val(str);
+    }
+
+})();
---
+++ kaboom-entropy/js/shaders/standardfragment.js
@@ -0,0 +1,164 @@
+varying vec2 vUv;
+        uniform float screenWidth;
+        uniform float screenHeight;
+        uniform sampler2D tSource;
+        uniform float delta;
+        uniform float feed;
+        uniform float kill;
+        
+        
+        
+        uniform vec2 brush;
+        uniform float brmode;
+
+        vec2 texel = vec2(1.0/screenWidth, 1.0/screenHeight);
+        float step_x = 1.0/screenWidth;
+        float step_y = 1.0/screenHeight;
+        
+        
+        
+  /* Simplex noise function */
+  vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+  float snoise(vec2 v) {
+    const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+                       -0.577350269189626, 0.024390243902439);
+    vec2 i  = floor(v + dot(v, C.yy) );
+    vec2 x0 = v -   i + dot(i, C.xx);
+    vec2 i1;
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+    i = mod(i, 289.0);
+    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+                     + i.x + vec3(0.0, i1.x, 1.0 ));
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),
+                                             dot(x12.zw,x12.zw)), 0.0);
+    m = m*m ;
+    m = m*m ;
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+    m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+    vec3 g;
+    g.x  = a0.x  * x0.x  + h.x  * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
+  }
+
+  vec2 homogen_uv()
+  {
+    float sqrt_F = sqrt(feed);
+    float U = 1.0;
+    float V = 0.0;
+    if (kill < (sqrt_F - 2.0 * feed) / 2.0) {
+      float A = sqrt_F / (feed + kill);
+      U = (A - sqrt(A*A - 4.0)) / (2.0 * A);
+      U = clamp(U, 0.0, 1.0);
+      V = sqrt_F * (A + sqrt(A*A - 4.0)) / 2.0;
+      V = clamp(V, 0.0, 1.0);
+    } // else, (U,V) already set to (1,0)
+    return vec2(U, V);
+  }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        void main()
+        {
+                      if (brush.x < -5.0) {
+                  /* This means the "clear" command was just given */
+                  vec2 huv = homogen_uv();
+                  if (brush.y < -5.0) {
+                    // Set to red state
+                    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); /* u=1, v=0, -, opaque */
+                  } else if (brush.y < 0.0) {
+                    // set to blue state
+                    gl_FragColor = vec4(huv.r, huv.g, 0.0, 1.0);
+                  } else if (brush.y == 2.0) {
+                    // let's do some noise
+                    // float t = 0.5*(snoise(0.21*vUv/texel) + snoise(0.34*vUv/texel));
+                    float t = 0.5*(snoise(0.1*vUv/texel) + snoise(0.16*vUv/texel));
+                    gl_FragColor = vec4(clamp(huv.r-t, 0.0, 1.0),
+                                        clamp(huv.g+t, 0.0, 1.0), 0.0, 1.0);
+                  } else {
+                    // works for most of the presets, but is worse for a couple of them
+                    float t1 = 0.5*(snoise(0.1*vUv/texel) + snoise(0.16*vUv/texel));
+                    float t2 = 0.5*(snoise(0.3-0.13*vUv/texel)
+                                                         + snoise(0.7-0.21*vUv/texel));
+                    gl_FragColor = vec4(clamp(huv.r-0.25+t1, 0.0, 1.0),
+                                        clamp(huv.g-0.25+t2, 0.0, 1.0), 0.0, 1.0);
+                  }
+                  return;
+                }
+            
+            //float feed = vUv.y * 0.083;
+            //float kill = vUv.x * 0.073;
+            
+         vec2 uv = texture2D(tSource, vUv).rg;
+            vec2 uv0 = texture2D(tSource, vUv+vec2(-step_x, 0.0)).rg;
+            vec2 uv1 = texture2D(tSource, vUv+vec2(step_x, 0.0)).rg;
+            vec2 uv2 = texture2D(tSource, vUv+vec2(0.0, -step_y)).rg;
+            vec2 uv3 = texture2D(tSource, vUv+vec2(0.0, step_y)).rg;               
+            vec2 lapl = (uv0 + uv1 + uv2 + uv3 - 4.0*uv);//10485.76;
+            
+            float Da;
+            float Db;
+            Da=0.2097;  // original
+            Db=0.105;    // original
+            
+        
+            
+            float du = /*0.00002*/Da*lapl.r - uv.r*uv.g*uv.g + feed*(1.0 - uv.r);
+            float dv = /*0.00001*/Db*lapl.g + uv.r*uv.g*uv.g - (feed+kill)*uv.g;
+
+
+            // enable this for circular cut-off
+            float R=sqrt( (vUv.x-0.5)*(vUv.x-0.5) + (vUv.y-0.5)*(vUv.y-0.5) );
+       //     if (R>0.4) {
+       //         //du=0.0;
+       //         dv=0.00;
+        //   }
+
+            vec2 dst = uv + delta*vec2(du, dv);
+            
+            if ((R>0.48 )|| (R<0.125)) {
+                dst.r=1.0;
+                dst.g=1.0;
+                //dv=0.00;
+           }
+
+
+            if(brush.x > 0.0)
+            {
+                vec2 diff = (vUv - brush)/texel;
+                float dist = dot(diff, diff);
+                if(dist < 5.0) {
+                    dst.g = 0.9;
+                    if (brmode > 0.0) {
+                        /* paint blue */
+                        dst.r = 0.0; dst.g = 0.9; /* set u=0, v=0.9 */
+                      } else {
+                        /* paint red */
+                        dst.r = 1.0; dst.g = 0.0; /* set u=1, v=0 */
+                      }
+                }
+            }
+            
+            gl_FragColor = vec4(dst.r, dst.g, 0.0, 1.0);
+        }
+        
+        
+        
+        
+        
+        
