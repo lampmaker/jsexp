5c4d1500cc7ec4410859ef207d3352dfd5af0a72
matthijs keuper <matthijs.keuper@gmail.com>
2020-10-15 18:50:13 +0200

    more test. exporting GLB works maar niet in google AR. -> rare meshes

--- kaboom-entropy/js/3DViewer/3dviewer.js
+++ kaboom-entropy/js/3DViewer/3dviewer.js
@@ -1,7 +1,7 @@
 import * as THREE from '/js/three/three.module.js';
 import { OrbitControls } from '/js/three/OrbitControls.js';
 import { SVGLoader } from '/js/three/SVGLoader.js';
-import {GLTFExporter} from '/js/three/GLTFExporter.js'
+import { GLTFExporter } from '/js/three/GLTFExporter.js'
 import { SimplifyModifier } from '/js/three/SimplifyModifier.js';
 
 var canvas;
@@ -15,15 +15,17 @@ var mMouseDown = false;
 // scene stuff
 var Renderer;
 var Scene;
-var Camera,controls, light0, light1;
-var SVGgeometry,SVGmesh, material;
+var Camera, controls, light0, light1;
+var SVGgeometry, SVGmesh, material;
 //=======================================================================================================
 //=======================================================================================================
-export function init(){
+
+
+export function init() {
     // init render ----------------------------------------------------------------------------------------------------
     canvasQ = $('#myCanvas');
     canvas = canvasQ.get(0);
-    Renderer = new THREE.WebGLRenderer({ canvas: canvas, preserveDrawingBuffer: true, antialias: true, precision: 'highp'});
+    Renderer = new THREE.WebGLRenderer({ canvas: canvas, preserveDrawingBuffer: true, antialias: true, precision: 'highp' });
     Scene = new THREE.Scene();
     Renderer.shadowMapEnabled = true;
     Renderer.shadowMapType = THREE.PCFSoftShadowMap;
@@ -32,18 +34,18 @@ export function init(){
     Renderer.toneMappingWhitePoint = .9;
     canvasWidth = canvasQ.width();
     canvasHeight = canvasQ.height()
-    
+
     //container.appendChild(renderer.domElement);   -- what i this? 
     // init camera ----------------------------------------------------------------------------------------------------
     Camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 100, 10000);
-    Camera.position.set(0,0,700);
-    
+    Camera.position.set(0, 0, 700);
+
     controls = new OrbitControls(Camera, Renderer.domElement);
     controls.screenSpacePanning = true;
-    resize(canvasWidth,canvasHeight);    //set everything right
+    resize(canvasWidth, canvasHeight);    //set everything right
     // init lights  ----------------------------------------------------------------------------------------------------
-    var L1 = 0xA0A0A0;
-	light0 = new THREE.AmbientLight(L1);
+    var L1 = 0x707070;
+    light0 = new THREE.AmbientLight(L1);
     Scene.add(light0);
 
     light1 = new THREE.SpotLight(0xFFFFFF - L1);
@@ -56,11 +58,11 @@ export function init(){
     light1.shadow.mapSize.height = light1.shadow.mapSize.width; // default
     light1.shadow.radius = 10;
 
-    light1.shadow.camera.near = 0.5;       // default
-    light1.shadow.camera.far = 3200;      // default
+    light1.shadow.camera.near = 5;       // default
+    light1.shadow.camera.far = 3000;      // default
     light1.shadow.camera.fov = 80;
     light1.shadowDarkness = 11;
-    light1.target.position.set(0,0,0);
+    light1.target.position.set(0, 0, 0);
     Scene.add(light1);
 
     // init background  ----------------------------------------------------------------------------------------------------
@@ -75,13 +77,13 @@ export function init(){
     var tablemesh = new THREE.Mesh(table, tablematerial);
     tablemesh.position.z = -1;
     tablemesh.receiveShadow = true;
-	tablemesh.castShadow = false;
-	Scene.add(tablemesh);
+    tablemesh.castShadow = false;
+    Scene.add(tablemesh);
 
     // init object   ----------------------------------------------------------------------------------------------------
     // init others  ----------------------------------------------------------------------------------------------------
-    var helper = new THREE.GridHelper(1000, 10);
-    helper.rotation.x = Math.PI / 2;
+    //var helper = new THREE.GridHelper(1000, 10);
+    //helper.rotation.x = Math.PI / 2;
     //scene.add(helper);
     animate();
 
@@ -95,79 +97,81 @@ export function resize(width, height) {
     canvasWidth = canvasQ.width();
     canvasHeight = canvasQ.height();
     //renderer.setPixelRatio(window.devicePixelRatio);
+
     Renderer.setSize(canvasWidth, canvasHeight);
-    Camera.aspect = canvasWidth/canvasHeight;
+    Camera.aspect = canvasWidth / canvasHeight;
     Camera.updateProjectionMatrix();
 }
 //=======================================================================================================
 //=======================================================================================================
-
-function savetoFile (data,filename,type){
+function savetoFile(data, filename, type) {
     var link = document.createElement('a');
     link.style.display = 'none';
     document.body.appendChild(link); // Firefox workaround, see #6594
-    var blob = new Blob([data],{ type: type });
+    var blob = new Blob([data], { type: type });
     link.href = URL.createObjectURL(blob);
-	link.download = filename;
-	link.click();
+    link.download = filename;
+    link.click();
 }
-
-export function export3D(){
+//=======================================================================================================
+//=======================================================================================================
+export function export3D() {
     console.log('exporting')
-    var _binary=true;
+    var _binary = false;
     var Exportscene = new THREE.Scene();
-    var simplifiedmesh=SVGmesh.clone();
 
-    const modifier = new SimplifyModifier();
-    var count = simplifiedmesh.geometry.attributes.position.count; // number of vertices to remove
-    console.log("Number of vertices at start:",count);
-   // simplifiedmesh.geometry = modifier.modify( simplifiedmesh.geometry, Math.floor(count/2 ));
-    count = Math.floor( simplifiedmesh.geometry.attributes.position.count); // number of vertices to remove
-    console.log("Number of vertices after simplification:",count);
-    Exportscene.add(simplifiedmesh);
+    SVGmesh.scale.x *= 0.01;
+    SVGmesh.scale.y *= 0.01;
+    SVGmesh.scale.z *= 0.01;
+    Exportscene.add(light0);
+
+    Exportscene.add(SVGmesh);
     console.log("LOG: export3d");
-    var options = {binary:_binary };
-    var exporter = new GLTFExporter();        
+
+    var options = { binary: _binary, forceindices: true };
+    var exporter = new GLTFExporter();
     exporter.parse(Exportscene, function (data) {
         console.log(data);
-        if(_binary){
-            savetoFile(data ,'test.glb','model/gltf-binary');
+        if (_binary) {
+            savetoFile(data, 'test.glb', 'model/gltf-binary');
         }
-        else {            
-            savetoFile(JSON.stringify(data) ,'test.gltf','text/plain');
+        else {
+            savetoFile(JSON.stringify(data), 'test.gltf', 'text/plain');
         }
-    }, 
-    options);
+    },
+        options);
 }
 
 
 //=======================================================================================================
 //=======================================================================================================
 
-export function resetview(cx,cy,cz){
+export function resetview(cx, cy, cz) {
     controls.reset();
-    Camera.position.set(cx,cy,cz);
-    
+    Camera.position.set(cx, cy, cz);
+
     Camera.updateProjectionMatrix();
 }
 //=======================================================================================================
 //=======================================================================================================
-export function loadSVG(url){
+export function loadSVG(url) {
     var loader = new SVGLoader();
-    var old=Scene.getObjectByName('svg');
+    var old = Scene.getObjectByName('svg');
     Scene.remove(old);
     Scene.children
-    console.log('OPENING SVG');    
+    console.log('OPENING SVG');
     loader.load(url, function (data) {
         var tpaths = data.paths;
-        console.log('opened');    
+        console.log('opened', data);
+        console.log('paths:', tpaths);
         var path = new THREE.ShapePath();
+
         for (var i = 0; i < tpaths.length; i++) {
             for (var j = 0; j < tpaths[i].subPaths.length; j++) {
                 path.subPaths.push(tpaths[i].subPaths[j]);
             }
         }
-       
+
         var texture = new THREE.TextureLoader().load("img/plywood1.jpg");
         texture.wrapS = THREE.RepeatWrapping;
         texture.wrapT = THREE.RepeatWrapping;
@@ -176,35 +180,34 @@ export function loadSVG(url){
         texture.center.y = 0;
         //texture.offset=(0,0);
         //var material= new THREE.MeshNormalMaterial();
-        var sidecolor=0x202020;
-        material = [
-            new THREE.MeshStandardMaterial({ map: texture, bumpMap: texture, color: 0xffAAAA }),
-            new THREE.MeshStandardMaterial({ map: texture,color: sidecolor }),
-            new THREE.MeshStandardMaterial({ map: texture,color: sidecolor })
-        ];
-        console.log('load shapes');    
+        var sidecolor = 0x202020;
+        var materialtop = new THREE.MeshStandardMaterial({ map: texture, bumpMap: texture, color: 0xffAAAA });
+        var basematerial = new THREE.MeshStandardMaterial({ color: 0xa00A0A });
+        var materialside = new THREE.MeshStandardMaterial({ color: sidecolor });
+        material = new THREE.MultiMaterial([materialtop, materialside]);
+        console.log('load shapes');
         var shapes = path.toShapes(true, false);
-        const modifier = new SimplifyModifier();   
+
         for (var j = 0; j < shapes.length; j++) {
             var shape = shapes[j];
             SVGgeometry = new THREE.ExtrudeBufferGeometry(shape, {
-               depth: 4,
-               bevelEnabled: true,
-               bevelThickness: 0.4,
-               bevelSize: 0.4,
-               steps:1,
-               BevelSegments:1,
-               curveSegments:5
+                depth: 0.004,
+                bevelEnabled: false,
+                bevelThickness: 0.4,
+                bevelSize: 0.4,
+                steps: 1,
+                BevelSegments: 2,
+                curveSegments: 3
             });
             SVGmesh = new THREE.Mesh(SVGgeometry, material);
-            SVGmesh.name='SVG';
+            SVGmesh.name = 'SVG';
             SVGmesh.castShadow = true;
-            SVGmesh.receiveShadow = true;            
+            SVGmesh.receiveShadow = true;
         }
-        console.log('shapes loaded');    
-         //-- repositioning 
-         SVGmesh.scale.y *= - 1;
-         SVGmesh.castShadow = true;
+        console.log('shapes loaded');
+        //-- repositioning 
+        SVGmesh.scale.y *= - 1;
+        SVGmesh.castShadow = true;
         var box = new THREE.BoxHelper(SVGmesh, 0xffff00);
         box.geometry.computeBoundingBox();
         var dimX = (box.geometry.boundingBox.max.x - box.geometry.boundingBox.min.x);
@@ -214,9 +217,10 @@ export function loadSVG(url){
         l = 590 / l;
         SVGmesh.scale.x *= l;
         SVGmesh.scale.y *= l;
+        SVGmesh.scale.z *= 1000;
         box.update();
 
-        console.log('Breed: ', dimX*l, ' , Hoog: ', dimY*l);
+        console.log('Breed: ', dimX * l, ' , Hoog: ', dimY * l);
 
         box.geometry.computeBoundingBox();
         var centerX = (box.geometry.boundingBox.max.x + box.geometry.boundingBox.min.x) / 2;
@@ -227,7 +231,11 @@ export function loadSVG(url){
         SVGmesh.position.z += 1;
         box.update();
         box.geometry.computeBoundingBox();
-        Scene.add(SVGmesh);        
+        Scene.add(SVGmesh);
+
+
+
+
     });
 }
 
@@ -239,7 +247,7 @@ function animate() {
 }
 //---------------------------------------------------------------------------------------------------------------------
 function render() {
-   Renderer.render(Scene, Camera);
+    Renderer.render(Scene, Camera);
 }
 
 //=======================================================================================================
